0490c90132010cf1355f1e194839e8017b58dfec
wengianche <wenchev1993@gmail.com>
2025-09-02 22:02:05 +0800

    CHARLS data exploration

--- Charls_demo.R
+++ Charls_demo.R
@@ -1,7 +1,7 @@
 #Project - MULTIMORBIDITY
 #BASED ON ANALYSIS PLAN_VERSION X CREATED BY PI WENG IAN CHE
 #CREATED: 20250822
-#UPDATED: 20250825
+#UPDATED: 20250827
 #ANALYST: WENG IAN CHE
 #PURPOSE OF THIS SYNTAX: EXPLORE CHARLS DATA STRUCTURE, DATA PREPARATION, PERFORM STATISTICAL ANALYSES 
 #R VERSION: version 4.4.3 (2025-02-28)
@@ -17,7 +17,11 @@
 ###################################################### 
 #250825 According to harmonized data (waves 1 to 4) documentation (mBfW3A-Harmonized_CHARLS_D), there are five individuals with contradictory information on interview status (RwIWSTAT varaibles)
 #250825 For missing values, check if missing coding is used or just plain missing (NA)
-
+#250826 QC: birth year ranging from 1900 and 2000 which is not reasonable and at least those born after 1973 should be excluded
+#250826 Variable: age at interview. At Waves 1 and 2, all respondents are asked to report their current age. Starting at Wave 3, respondents are not asked to report their current age. Then, create a new age at interview variable by (interview year-birth year)
+#250826 Variable: Hukou (R3HUKOU). There are around 1,000 re-interview respondents in wave 3 with missing values for RwHUKOU who disputed the previous wave hukou type but do not have a corrected type available.
+#250826 Variable: self-reported health (RwSHLT and RwSHLTA (preferable given it is available across the waves), with many missing values). CHARLS has two scales for respondents to self-report their current health condition. Starting at Wave 4, respondents were only asked to self-report their current health condition in the beginning of the health module. Respondents are asked the second scale.
+#250827, Variable: ADL assessment only among to those >=50 years and reported good health. However, some individuals younger than 50y also had values for these variables
 ######################################################
 
 #Outline
@@ -25,10 +29,13 @@
 #INSTALL AND LOAD LIBRARY
 
 #DATA EXPLORATION
-#1. Participation of individuals across waves
+#1. Create a variable list 
+#2. Participation of individuals across waves (using harmonized data)
+#3. Drop out and death statuses across waves (using harmonized data)
 #2. Missing values
 
 #DATA PREPARATION
+#1. Adding variables from wave 5
 ######################################################  
 ###################################################### 
 #INSTALL AND LOAD LIBRARY
@@ -39,17 +46,51 @@ pacman::p_load(Hmisc, #attach labels to variables
                tidyverse,
                ggalluvial,
                ggplot2,
-               plotly
+               plotly,
+               labelled,
+               openxlsx
 ) 
 ###################################################### 
 
 ######################################################
 #DATA EXPLORATION
-#1. Participation of individuals across waves (using harmonized data)
-#2. Drop out and death statuses across waves (using harmonized data)
+#1. Create a variable list 
+#2. Participation of individuals across waves (using harmonized data)
+#3. Drop out and death statuses across waves (using harmonized data)
 #2. Missing values
 ######################################################
-#1. Participation of individuals across waves (using harmonized data)
+#1. Create a variable list 
+######################################################
+#Load data
+#Harmonized data_waves_1_to_4
+setwd('/Users/wengianche/Documents/UM MACAO FELLOW/Data/CHARLS/Waves/Harmonized CHARLS_waves_1_to_4')
+hw14_full <- read_dta("H_CHARLS_D_Data.dta") #missing codes in the original dta file are all collapsed to "NA"
+
+#Create a detailed table
+hw14_variable_table <- data.frame(
+  Variable = names(hw14_full),
+  Label = sapply(var_info, function(x) ifelse(is.null(x), "", x)),
+  Type = sapply(hw14_full, function(x) paste(class(x), collapse = ", ")),
+  N_Missing = colSums(is.na(hw14_full)),
+  Unique_Values = sapply(hw14_full, function(x) length(unique(na.omit(x)))),
+  stringsAsFactors = FALSE
+)
+
+#Add value labels as a separate column
+hw14_variable_table$Value_Labels <- sapply(names(hw14_full), function(var) {
+  labels <- val_info[[var]]
+  if(!is.null(labels)) {
+    paste(paste0(names(labels), " (", labels, ")"), collapse = "; ")
+  } else {
+    ""
+  }
+})
+
+#Save as excel file
+setwd('/Users/wengianche/Documents/UM MACAO FELLOW/Data/CHARLS/Basic information')
+write.xlsx(hw14_variable_table, file = "hw14_variable_table_250825.xlsx", colNames=T, format_headers=T)
+######################################################
+#2. Participation of individuals across waves (using harmonized data)
 ######################################################
 #Load data
 #Harmonized data_waves_1_to_4
@@ -146,7 +187,7 @@ dev.off()
 #Wave3: 3394
 #Wave4: 470
 ######################################################
-#2. Drop out and death statuses across waves (using harmonized data)
+#3. Drop out and death statuses across waves (using harmonized data)
 ######################################################
 #Load data
 #Harmonized data_waves_1_to_4
@@ -170,6 +211,39 @@ setwd('/Users/wengianche/Documents/UM MACAO FELLOW/Data/CHARLS/Waves/CHARLS2011r
 setwd('/Users/wengianche/Documents/UM MACAO FELLOW/Data/CHARLS/Waves/CHARLS2020r')
 w5 <- read_dta("Sample_Infor.dta") %>% select(1,)
 ######################################################
+#DATA PREPARATION
+#1. Adding variables from wave 5
+######################################################  
+#1. Adding variables from wave 5
+###################################################### 
+#Variables to be added
+#Weight (housefold and individual levels)
+#Interview Dates
+#Birth date: Month, and Year (compare to wave 5 variables and update, focus on RAIDBYEAR, RAIDBMONTH and RAIDBDAY)
+#Death: Month, and Year, The Wave 3 and 4 exit interview files have not been released. Extract from wave 3 to 5?
+#Age at interview (in months and years)
+#Education (update needed?)
+#Current Marital Status and Number of Marriage
+#HuKou, Live in Urban or Rural, Lives in Institution at Interview
+#Self-report of Health
+#Activities of daily living (ADLs): Some difficulty, Instrumental activities of daily living (IADLs): Some difficulty, Other Functional Limitations: Some difficulty, ADL summary variable (use this only?)
+#Doctor diagnosed health problems: Ever Had Condition, Doctor diagnosed health problems: Memory-related disease, Whether Receives Treatment or Medication for Disease (update as well after dispute disease diagnosis? should be as variables regarding disease record verification were included to build these variables)
+#Health behaviors: Physical Activity or Exercise (with many missing as only half of sample were randomly selected to answer these questions in the first three waves, only included for wave 4?)
+#Health behaviors: Drinking
+#Health behaviors: Smoking (cigarettes)
+#Medical Care Utilization: Inpatient/Hospital Stay, Medical Care Utilization: Outpatient/ doctor visit, Medical Care Utilization: Dental visit (exclude as data only available at waves 2 and 3?), Traditional medicine facility visit
+#Medical Expenditure: hospitalization in the past year,  dental visit in the last year (also only for waves 2 and 3)
+#Covered by Public Health Insurance Program (as covariates), Private Health Insurance Program, Other Health Insurance Program
+#Self-reported Memory, Immediate Word Recall, Delayed Word Recall, Date Naming (with many missing values, esp for wave 2), Drawing Picture, Total Recall Summary Score
+#Inflation Multiplier
+#Individual value: cash and savings accounts, stocks and mutual funds (with many missing values at waves 2 to 4), government bonds, all other savings (with many missing at waves 2 to 4), other debt (with many missing at waves 2 to 4), Total non-housing financial wealth
+#Household value: other house (not primary residence) (with many missing at waves 2 to 4), primary residence, total wealth (household level available for waves 1 and 2 only, as covariate/converted to couple level?)
+#Earned Income (with many missing at waves 2 to 4), Total Capital Income (with many missing at waves 2 to 4), Pension Income
+#Government/Public Transfer Income (with many missing at waves 2 to 4), Other Income (with many missing at waves 2 to 4), Income from Other Household Members, 
+#Household food consumption (with many missing at waves 2 to 4), Household non-food consumption in last month (with many missing at waves 2 to 4), Household other non-food consumption in last year (with many missing at waves 2 to 4), Total household consumption
+#Number of People Living in Household, Living Children, Deceased Children (as adverse psychological event?), Living Sibling, Deceased Sibling, Parental Mortality (dispute may happen after wave 2 but correction was not done, with many missing at waves 2 to 4), Parental SES: Education and Harmonized Education 
+###################################################### 
+###################################################### 
 
 
 # Read the Stata file
